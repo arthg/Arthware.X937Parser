cd32cde1ac8634964e3c7adc0a7948608cceba49
Arthur Gorr <art.gorr@gmail.com>
2017-08-08 22:55:54 -0500

    First failing test

---
+++ Arthware.X937Parser/Arthware.X937Parser.Tests/X937ParserTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System.IO;
+
+namespace Arthware.X937Parser.Tests
+{
+    [TestFixture]
+    public abstract class X937ParserTests
+    {
+        private X937Parser _x937Parser;
+        private Stream _stream;
+
+        [SetUp]
+        public void PrepareX937ParserTests()
+        {
+            _stream = new MemoryStream();
+            _x937Parser = new X937Parser();
+        }
+
+        public sealed class GetX937ReturnsMethod : X937ParserTests
+        {
+            [Test]
+            public void Should_()
+            {
+                //arrange
+                //act
+                var result = _x937Parser.GetX937Returns(_stream);
+
+                //assert
+            }
+        }
+    }
+}
---
+++ Arthware.X937Parser/Arthware.X937Parser/X937Parser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Arthware.X937Parser.Models;
+
+namespace Arthware.X937Parser
+{
+    public sealed class X937Parser : IX937FileParser
+    {
+        public IEnumerable<X937Return> GetX937Returns(Stream stream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
